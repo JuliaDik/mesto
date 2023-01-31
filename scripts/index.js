1d7589afacf4a3b9962cb2dac099fb8533d31ff6
Julia Dikolenko <tsvetkova-julia-vl@yandex.ru>
2023-01-31 05:16:17 +0300

    отформатировать итоговую версию

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+// Открытие и закрытие попапа
+let editButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let closeButton = document.querySelector('.popup__close-button');
+
+// Открытие попапа
+function popupOpened() {
+  popup.classList.add('popup_opened');
+}
+editButton.addEventListener('click', popupOpened);
+
+// Закрытие попапа
+function popupClosed() {
+  popup.classList.remove('popup_opened');
+}
+closeButton.addEventListener('click', popupClosed);
+
+// Редактирование профиля (имени и информации о себе)
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.form__text_type_name');
+let jobInput = document.querySelector('.form__text_type_occupation');
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    let nameProfile = document.querySelector('.profile__name');
+    let jobProfile = document.querySelector('.profile__occupation');
+    nameProfile.textContent = nameInput.value;
+    jobProfile.textContent = jobInput.value;
+}
+
+let saveButton = document.querySelector('.form__save-button');
+saveButton.addEventListener('click', popupClosed);
+
+formElement.addEventListener('submit', handleFormSubmit);
+
+
