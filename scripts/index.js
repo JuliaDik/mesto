2289806771693ae3638822bb5e83acbbf907dbab
Julia Dikolenko <tsvetkova-julia-vl@yandex.ru>
2023-02-13 02:57:51 +0300

    feat: добавить открытие и закрытие попапов (edit и add)

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,6 @@
-const nameProfile = document.querySelector('.profile__name');
-const occupationProfile = document.querySelector('.profile__occupation');
-const editionButton = document.querySelector('.profile__edit-button');
-const popup = document.querySelector('.popup');
-const formElement = document.querySelector('.popup__edit-form');
-const nameInput = document.querySelector('.popup__input_type_name');
-const occupationInput = document.querySelector('.popup__input_type_occupation');
-const closureButton = document.querySelector('.popup__close-button');
-const cards = document.querySelector('.cards');
+// добавить 6 карточек из массива
+const cardsGallery = document.querySelector('.cards');
 const cardTemplate = document.querySelector('.card-template').content;
-
 const initialCards = [
   {
     name: 'Архыз',
@@ -36,36 +28,59 @@ const initialCards = [
   }
 ];
 
-// Открытие попапа
-function openPopup() {
+initialCards.forEach(item => {
+  const cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector('.card__title').textContent = item.name;
+  cardElement.querySelector('.card__image').src = item.link;
+  cardsGallery.append(cardElement);
+});
+
+// открыть попап
+const popupEdit = document.querySelector('.popup_type_edit');
+const popupAdd = document.querySelector('.popup_type_add');
+const nameProfile = document.querySelector('.profile__name');
+const occupationProfile = document.querySelector('.profile__occupation');
+const nameInput = document.querySelector('.popup__input_type_name');
+const occupationInput = document.querySelector('.popup__input_type_occupation');
+const titleInput = document.querySelector('.popup__input_type_title');
+const linkInput = document.querySelector('.popup__input_type_link');
+const buttonEdit = document.querySelector('.profile__edit-button');
+const buttonAdd = document.querySelector('.profile__add-button');
+function openPopup(popup) {
   popup.classList.add('popup_opened');
+}
+
+function openEditPopup() {
   nameInput.value = nameProfile.textContent;
   occupationInput.value = occupationProfile.textContent;
+  openPopup(popupEdit);
 }
 
-// Закрытие попапа
+function openAddPopup() {
+  titleInput.value = '';
+  linkInput.value = '';
+  openPopup(popupAdd);
+}
+
+buttonEdit.addEventListener('click', openEditPopup);
+buttonAdd.addEventListener('click', openAddPopup);
+
+// закрыть попап
+const popups = document.querySelectorAll('.popup');
+const buttonsClose = document.querySelectorAll('.popup__close-button');
 function closePopup() {
-  popup.classList.remove('popup_opened');
+  const index = Array.from(buttonsClose).indexOf(this);
+  popups[index].classList.remove('popup_opened');
 }
 
-// Сохранение и отправка формы
+buttonsClose.forEach(btn => btn.addEventListener('click', closePopup));
+
+// сохранить и отправить форму редактирования профиля
+const formElement = document.querySelector('.popup__edit-form');
 function handleFormSubmit (evt) {
-    evt.preventDefault();
-    nameProfile.textContent = nameInput.value;
-    occupationProfile.textContent = occupationInput.value;
-    closePopup();
+  evt.preventDefault();
+  nameProfile.textContent = nameInput.value;
+  occupationProfile.textContent = occupationInput.value;
+  popupEdit.classList.remove('popup_opened');
 }
-
-editionButton.addEventListener('click', openPopup);
-closureButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', handleFormSubmit);
-
-// Добавление карточек из массива initialCards
-initialCards.forEach(function (element) {
-  const cardElement = cardTemplate.cloneNode(true);
-
-  cardElement.querySelector('.card__title').textContent = element.name;
-  cardElement.querySelector('.card__image').src = element.link;
-
-  cards.append(cardElement);
-});
